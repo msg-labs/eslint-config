33148ef5a80d7739db9dbc76480c964547029363
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2018-09-12 19:23:25 +0100

    feat: add rules for variables

---
+++ rules/variables.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'init-declarations': [
+        'error',
+        'always'
+    ],
+    'no-delete-var': [ 'error' ],
+    'no-label-var': [ 'error' ],
+    'no-shadow': [ 'error' ],
+    'no-shadow-restricted-names': [ 'error' ],
+    'no-undef': [ 'error' ],
+    'no-undef-init': [ 'warn' ],
+    'no-undefined': [ 'error' ],
+    'no-unused-vars': [ 'error' ],
+    'no-use-before-define': [ 'error' ]
+};
