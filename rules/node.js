d7027ead84737c32392840f7a82e24980d667b41
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2018-09-13 22:54:48 +0100

    feat: nodejs rules

---
+++ rules/node.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'callback-return': [ 'error' ],
+    'global-require': [ 'error' ],
+    'handle-callback-err': [ 'warn' ],
+    'no-buffer-constructor': [ 'error' ],
+    'no-mixed-requires': [ 'error' ],
+    'no-new-require': [ 'error' ],
+    'no-path-concat': [ 'error' ],
+    'no-process-env': [ 'error' ],
+    'no-process-exit': [ 'error' ],
+    'no-sync': [ 'warn' ]
+};
