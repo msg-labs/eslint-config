d95645ad6f8d9be780a7de3cac534c1a3a19acfb
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2018-09-16 21:33:22 +0100

    feat: actually use .eslintrc to lint the project and leave index.js for exporting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,6 @@
-const errors = require( './rules/possible-errors.js' );
-const bestPractices = require( './rules/best-pratices.js' );
-const rules = require( './rules/rules.js' );
-const node = require( './rules/node.js' );
-const stylisticIssues = require( './rules/stylistic-issues.js' );
-const es6 = require( './rules/es6.js' );
-const variables = require( './rules/variables.js' );
-
 module.exports = {
-    parserOptions: {
-        ecmaVersion: 9,
-        sourceType: 'module'
+    env: {
+        node: true
     },
-    rules: {
-        ...errors,
-        ...bestPractices,
-        ...rules,
-        ...node,
-        ...stylisticIssues,
-        ...es6,
-        ...variables
-    }
+    extends: './index.js'
 };
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-module.exports = require( './.eslintrc' );
+const errors = require( './rules/possible-errors.js' );
+const bestPractices = require( './rules/best-pratices.js' );
+const rules = require( './rules/rules.js' );
+const node = require( './rules/node.js' );
+const stylisticIssues = require( './rules/stylistic-issues.js' );
+const es6 = require( './rules/es6.js' );
+const variables = require( './rules/variables.js' );
+
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 9,
+        sourceType: 'module'
+    },
+    rules: {
+        ...errors,
+        ...bestPractices,
+        ...rules,
+        ...node,
+        ...stylisticIssues,
+        ...es6,
+        ...variables
+    }
+};
