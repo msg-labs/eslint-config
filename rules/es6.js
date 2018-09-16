b103cca3c12e2d00b422e26062e36eda4bc5b255
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2018-09-16 21:20:17 +0100

    feat: adds es6 related rules

---
+++ rules/es6.js
@@ -0,0 +1,65 @@
+module.exports = {
+    'arrow-body-style': [
+        'error',
+        'as-needed'
+    ],
+    'arrow-parens': [
+        'error',
+        'as-needed'
+    ],
+    'arrow-spacing': [
+        'error',
+        {
+            before: true,
+            after: true
+        }
+    ],
+    'constructor-super': [ 'error' ],
+    'generator-star-spacing': [
+        'error',
+        'after'
+    ],
+    'no-class-assign': [ 'error' ],
+    'no-confusing-arrow': [ 'error' ],
+    'no-const-assign': [ 'error' ],
+    'spaced-comment': [ 'error' ],
+    'no-duplicate-imports': [
+        'error',
+        { includeExports: true }
+    ],
+    'no-new-symbol': [ 'error' ],
+    'no-this-before-super': [ 'error' ],
+    'no-useless-computed-key': [ 'error' ],
+    'no-useless-constructor': [ 'error' ],
+    'no-useless-rename': [ 'error' ],
+    'no-var': [ 'error' ],
+    'object-shorthand': [
+        'error',
+        'always',
+        {
+            avoidExplicitReturnArrows: true
+        }
+    ],
+    'prefer-arrow-callback': [ 'error' ],
+    'prefer-const': [ 'error' ],
+    'prefer-destructuring': [ 'error' ],
+    'prefer-numeric-literals': [ 'off' ],
+    'prefer-rest-params': [ 'error' ],
+    'prefer-spread': [ 'error' ],
+    'prefer-template': [ 'error' ],
+    'require-yield': [ 'error' ],
+    'rest-spread-spacing': [
+        'error',
+        'never'
+    ],
+    'sort-imports': [ 'off' ],
+    'symbol-description': [ 'error' ],
+    'template-curly-spacing': [
+        'error',
+        'always'
+    ],
+    'yield-star-spacing': [
+        'error',
+        'after'
+    ]
+};
