bf0023567a3acd13f7efc4ff2aede080b03dc768
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2019-12-12 23:25:53 +0100

    fix: improves test overrides by loosing eslint a bit

--- rules/test-overrides.js
+++ rules/test-overrides.js
@@ -1,7 +1,10 @@
 module.exports = {
+    'max-lines': 'off',
+    'max-lines-per-function': 'off',
     'newline-per-chained-call': [
         'error',
         { ignoreChainWithDepth: 2 }
     ],
-    'no-magic-numbers': 'off'
+    'no-magic-numbers': 'off',
+    'no-undefined': 'off'
 };
